a65e1371c71ded13af2a5a613dc7091f97b438a3
kitixis5556 <leutaiparxoun@gmail.com>
2025-11-07 00:07:48 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{
+      textAlign: 'center',
+      padding: '50px',
+      fontFamily: "'Libre Bodoni', serif",
+      backgroundColor: '#443627',
+      color: 'white',
+      minHeight: '100vh'
+    }}>
+      <h1>Bakero Barbershop - Ξάνθη</h1>
+      <p>Η αυθεντική τέχνη της ανδρικής περιποίησης</p>
+    </div>
+  )
+}
+
+export default App
